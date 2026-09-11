4457df4ef9c1c3ddb90743f095d5d675f0fe0ef6
sellfmedia-del <sellfmedia@gmail.com>
2026-09-11 17:37:34 +0300

    Assemble BHS report visual asset

---
+++ data/bhsReportAsset.ts
@@ -0,0 +1,8 @@
+import chunk1 from "./bhsReportAssetChunk1";
+import chunk2 from "./bhsReportAssetChunk2";
+import chunk3 from "./bhsReportAssetChunk3";
+import chunk4 from "./bhsReportAssetChunk4";
+import chunk5 from "./bhsReportAssetChunk5";
+import chunk6 from "./bhsReportAssetChunk6";
+
+export const bhsReportDataUri = `data:image/webp;base64,${chunk1}${chunk2}${chunk3}${chunk4}${chunk5}${chunk6}`;
