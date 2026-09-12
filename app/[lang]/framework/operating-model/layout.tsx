ff9d07f7c1446520fe3218572cd69f118c16b456
sellfmedia-del <sellfmedia@gmail.com>
2026-09-12 12:27:31 +0300

    Add isolated Operating Model visual refinement layer

---
+++ app/[lang]/framework/operating-model/layout.tsx
@@ -0,0 +1,5 @@
+import "./visual-refine.css";
+
+export default function OperatingModelLayout({ children }: { children: React.ReactNode }) {
+  return <div className="opModelVisualRefine">{children}</div>;
+}
