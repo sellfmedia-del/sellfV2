669f004434ed303983394da742979ef74b9ccdb6
sellfmedia-del <sellfmedia@gmail.com>
2026-09-12 13:25:52 +0300

    Load mockup fidelity overrides for Operating Model

--- app/[lang]/framework/operating-model/layout.tsx
+++ app/[lang]/framework/operating-model/layout.tsx
@@ -1,4 +1,5 @@
 import "./visual-refine.css";
+import "./mockup-fidelity.css";
 
 export default function OperatingModelLayout({ children }: { children: React.ReactNode }) {
   return <div className="opModelVisualRefine">{children}</div>;
