608065d2c8bf05b8d14157d09860a0f189198b37
sellfmedia-del <sellfmedia@gmail.com>
2026-09-17 12:37:18 +0300

    refactor: remove the temporary standalone Engage Studio

--- app/(studio)/engage-studio/[[...tool]]/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import {NextStudio} from 'next-sanity/studio'
-import config from '@/engage-studio/sanity.config'
-
-export default function EngageStudioPage() {
-  return <NextStudio config={{...config, basePath: '/engage-studio'}} />
-}
