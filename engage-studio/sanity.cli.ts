fa99e9b29c4b1823d09c6e14780dee3b59726a6a
sellfmedia-del <sellfmedia@gmail.com>
2026-09-17 14:30:33 +0300

    Pin Engage Studio deployment target

--- engage-studio/sanity.cli.ts
+++ engage-studio/sanity.cli.ts
@@ -4,4 +4,7 @@ import {dataset, projectId} from '../sanity/env'
 export default defineCliConfig({
   api: {projectId, dataset},
   studioHost: 'sellf-engage',
+  deployment: {
+    appId: 'uu5aggs7o80g458iawusym9q',
+  },
 })
