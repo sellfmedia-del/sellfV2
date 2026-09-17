54529d84c46a98b23dca4a38ea570d7f64afb9bd
sellfmedia-del <sellfmedia@gmail.com>
2026-09-17 12:52:25 +0300

    revert: keep Engage as an independent Sanity Studio

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import category from './category'
 import post from './post'
-import {engageSchema} from '../engage/schemaTypes'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [category, post, ...engageSchema.types],
+  types: [category, post],
 }
